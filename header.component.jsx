c67421993c3d893de4ebb13860804f0a7b482fbe
Kennedy <kennedylie@outlook.com>
2020-06-25 16:40:22 +0700

    Add files via upload

---
+++ header.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.styles.scss';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils';
+import {connect} from 'react-redux';
+
+const Header = ({currentUser}) => (
+    <div className='header'>
+        <Link to="/" className='logo-container'>
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link to='/shop' className='option'>SHOP</Link>
+            <Link to='/shop' className='option'>CONTACT</Link>
+            {
+                currentUser ?
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                :
+                <Link to='/signin' className='option'>SIGN IN</Link>
+            }
+        </div>
+    </div>
+);
+
+const mapStateToProps = (state) =>({
+    currentUser:state.user.user,
+})
+
+export default connect(mapStateToProps)(Header);
